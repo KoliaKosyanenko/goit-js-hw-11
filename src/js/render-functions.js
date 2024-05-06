61ac4f2177597a36cfb29a65d5732dc0115e070d
KoliaKosyanenko <112539586+KoliaKosyanenko@users.noreply.github.com>
2024-05-06 20:34:51 +0300

    Updated js

---
+++ src/js/render-functions.js
@@ -0,0 +1,34 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+export function renderGallery(images) {
+  const galleryElement = document.getElementById('gallery');
+  galleryElement.innerHTML = '';
+
+  images.forEach(image => {
+    const cardHTML = `
+      <div class="gallery">
+        <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" class="card-img-top"></a>
+        <div class="card-body">
+          <h5 class="card-title">${image.tags}</h5>
+          <p class="card-text">Likes: ${image.likes}</p>
+          <p class="card-text">Views: ${image.views}</p>
+          <p class="card-text">Comments: ${image.comments}</p>
+          <p class="card-text">Downloads: ${image.downloads}</p>
+        </div>
+      </div>
+    `;
+    galleryElement.insertAdjacentHTML('beforeend', cardHTML);
+  });
+    
+    new SimpleLightbox('.gallery');
+}
+
+export function showError(message) {
+  iziToast.error({
+    title: 'Error',
+    message: message,
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { searchImages } from './js/pixabay-api.js';
+import { showError, renderGallery } from './js/render-functions.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const searchForm = document.getElementById('search-form');
+  searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const searchInput = document.getElementById('search-input');
+    const query = searchInput.value.trim();
+
+    if (query !== '') {
+      searchImages(query)
+        .then(images => {
+          if (images.length === 0) {
+            showError(
+              'Sorry, there are no images matching your search query. Please try again!'
+            );
+          } else {
+            renderGallery(images);
+          }
+        })
+        .catch(error => {
+          showError(
+            'An error occurred while fetching data. Please try again later.'
+          );
+        });
+    } else {
+      showError('Please enter a search query.');
+    }
+  });
+});
